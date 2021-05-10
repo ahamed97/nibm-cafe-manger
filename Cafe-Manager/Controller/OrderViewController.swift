02c3fe0f48e8e18b14772a56a668aa2b04dafba9
dev <dev@devs-iMac.local>
2021-05-09 23:27:06 -0700

    order status manually change updated

--- Cafe-Manager/Controller/OrderViewController.swift
+++ Cafe-Manager/Controller/OrderViewController.swift
@@ -12,11 +12,11 @@ import Firebase
 class OrderViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 let ref = Database.database().reference()
     @IBOutlet weak var tblOrders: UITableView!
-    @IBOutlet weak var segTabs: UISegmentedControl!
-    
+        
     var orders: [Order] = []
     var filteredOrders: [Order] = []
     
+    var selectedSagSt = 0
 
     override func viewDidLoad() {
           super.viewDidLoad()
@@ -28,10 +28,13 @@ let ref = Database.database().reference()
     override func viewDidAppear(_ animated: Bool) {
           self.fetchOrders()
     }
-
-    @IBAction func onStatusSegChanged(_ sender: UISegmentedControl) {
-      filterOrders(status: sender.selectedSegmentIndex)
+    
+    
+    @IBAction func onStatusSegChange(_ sender: UISegmentedControl) {
+        filterOrders(status: sender.selectedSegmentIndex)
+        selectedSagSt = sender.selectedSegmentIndex
     }
+    
     
     func filterOrders(status: Int) {
         filteredOrders.removeAll()
@@ -77,7 +80,7 @@ let ref = Database.database().reference()
                     }
                     
                     self.filteredOrders.append(contentsOf: self.orders)
-                    self.onStatusSegChanged(self.segTabs)
+                    self.filterOrders(status: 0)
                 } else {
                     let alert = UIAlertController(title: "Info", message: "No Orders", preferredStyle: UIAlertController.Style.alert)
                     alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
@@ -100,7 +103,97 @@ let ref = Database.database().reference()
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
-
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        [UITableViewRowAction?]()
+        let btnPreparing = UITableViewRowAction(style: .normal,title:"Preparing"){
+            (rowAction,indexPath) in
+            self.changeOrderStatus(orderId :self.filteredOrders[indexPath.row].orderID,status :1)
+        }
+        let btnReady = UITableViewRowAction(style: .normal,title:"Ready"){
+            (rowAction,indexPath) in
+            self.changeOrderStatus(orderId :self.filteredOrders[indexPath.row].orderID,status :2)
+        }
+        let btnDone = UITableViewRowAction(style: .normal,title:"Done"){
+            (rowAction,indexPath) in
+            self.changeOrderStatus(orderId :self.filteredOrders[indexPath.row].orderID,status :3)
+        }
+        let btnArrive = UITableViewRowAction(style: .normal,title:"Arrive"){
+            (rowAction,indexPath) in
+            self.changeOrderStatus(orderId :self.filteredOrders[indexPath.row].orderID,status :4)
+        }
+        let btnCancel = UITableViewRowAction(style: .normal,title:"Cancel"){
+            (rowAction,indexPath) in
+            self.changeOrderStatus(orderId :self.filteredOrders[indexPath.row].orderID,status :5)
+        }
+        let btnDelete = UITableViewRowAction(style: .normal,title:"Delete"){
+            (rowAction,indexPath) in 
+            self.deleteOrder(orderId :self.filteredOrders[indexPath.row].orderID)
+        }
+        btnPreparing.backgroundColor = UIColor.black
+        btnReady.backgroundColor = UIColor.brown
+        btnDone.backgroundColor = UIColor.systemIndigo
+        btnArrive.backgroundColor = UIColor.gray
+        btnCancel.backgroundColor = UIColor.red
+        btnDelete.backgroundColor = UIColor.red
+        
+        if(selectedSagSt == 0){
+            return [btnPreparing,btnCancel]
+        }
+        else if(selectedSagSt == 1){
+            return [btnReady,btnCancel]
+        }
+        else if(selectedSagSt == 2){
+            return [btnDone,btnCancel]
+        }
+        else if(selectedSagSt == 3){
+            return [btnArrive,btnDelete]
+        }
+        else {
+            return [btnDelete]
+        }
+    }
+    
+    func changeOrderStatus(orderId: String, status: Int) {
+            ref
+                .child("orders")
+                .child(orderId)
+                .child("status")
+                .setValue(status) {
+                    error, reference in
+                    if error != nil {
+    let alert = UIAlertController(title: "Error", message: "Something went wrong", preferredStyle: UIAlertController.Style.alert)
+    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+    self.present(alert, animated: true, completion: nil)
+                    }
+                }
+        filterOrders(status: status)
+        }
+    
+    func deleteOrder(orderId: String) {
+            ref
+                .child("orders")
+                .child(orderId)
+                .removeValue() {
+                                error, databaseReference in
+                                if error != nil{
+                                    if let error = error {
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                            }
+                                }
+                }
+        filterOrders(status: 0)
+        }
+    
 }
 
 
